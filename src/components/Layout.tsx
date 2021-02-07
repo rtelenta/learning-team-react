35f4dfe0d23a30899c375490a73672a3fef092f0
Renzo Telenta <renzotelenta@gmail.com>
2021-02-06 22:40:27 -0500

    styles: add padding

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/react";
 
 const Layout: React.FC = ({ children }) => {
   return (
-    <Box mx="auto" maxW="6xl" px={4}>
+    <Box mx="auto" maxW="4xl" px={4}>
       {children}
     </Box>
   );
